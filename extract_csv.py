10ca03445e5569cb49cf51ab1c006fcc70b046cf
leandrovaladao <leandro.costa@starlinetecnologia.com.br>
2020-12-19 13:30:31 -0300

    adiciona resposta da letra A e B da task 2

--- extract_csv.py
+++ extract_csv.py
@@ -2,8 +2,8 @@ from pandas import read_csv
 
 
 def extract_file_x():
-    return read_csv('X.csv')
+    return read_csv('../X.csv')
 
 
 def extract_file_y():
-    return read_csv('Y.csv')
+    return read_csv('../Y.csv')
